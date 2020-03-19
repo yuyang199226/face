a46c84243f0e8598c2cd7538b9bfb07b7ad1b8e4
yangyu10 <yangyu@aibee.com>
2020-03-19 16:02:59 +0800

    3d 可视化

--- vis.py
+++ vis.py
@@ -4,6 +4,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Fixing random state for reproducibility
+def three_D(dataSet):
+    print("3D display...")
+    from mpl_toolkits.mplot3d import Axes3D
+    fig=plt.figure()
+    ax=Axes3D(fig)
+    ax.scatter(dataSet[:,0], dataSet[:,1], dataSet[:,2])
+    plt.show()
+
+
 
 def display(dataSet):
     for c_id, d in dataSet.items():
@@ -23,3 +32,15 @@ def display(dataSet):
 
     plt.legend(loc='upper left')
     plt.show()
+
+
+if __name__ == '__main__':
+    dataSet = [
+            [0,1,0],
+            [1,1,1],
+            [2,1,3],
+            [4,1,9],
+            [5,2,12],
+            [3,-3,6],
+            ]
+    three_D(np.asarray(dataSet))
